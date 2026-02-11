e75f4a512fdf50a5bdd2b7906e8b2b57675bd40b
Wiktor Wijata <rescuepc.software@gmail.com>
2026-02-11 16:45:39 +0100

    #53 prepare Portfolio.Web to SPA

--- Source/Portfolio.Web/Program.cs
+++ Source/Portfolio.Web/Program.cs
@@ -1,23 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSpaStaticFiles(configuration =>
-{
-    configuration.RootPath = "ClientApp/dist";
-});
-
 var app = builder.Build();
 
 app.UseStaticFiles();
-app.UseSpaStaticFiles();
 
-app.UseSpa(spa =>
-{
-    spa.Options.SourcePath = "ClientApp";
-
-    if (app.Environment.IsDevelopment())
-    {
-        spa.UseProxyToSpaDevelopmentServer("http://localhost:5173");
-    }
-});
+app.MapFallbackToFile("index.html");
 
 app.Run();
